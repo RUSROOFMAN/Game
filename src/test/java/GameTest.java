d996c48318fc99837b00008716f12017f7ab9110
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-04-19 22:07:59 +0700

    Change code

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -13,4 +13,48 @@ public void testWhenSecondPlayerWin() {
         int expected = 2;
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    public void testWhenFirstPlayerWin(){
+        Player Petya = new Player(1, "Петя", 100);
+        Player Vasya = new Player(2, "Вася", 120);
+        Game game = new Game();
+        game.registered(Vasya);
+        game.registered(Petya);
+        int actual = game.round("Вася", "Петя");
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testWhenStrengthsEquals(){
+        Player Petya = new Player(1, "Петя", 100);
+        Player Vasya = new Player(2, "Вася", 100);
+        Game game = new Game();
+        game.registered(Vasya);
+        game.registered(Petya);
+        int actual = game.round("Вася", "Петя");
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testWhenFirstPlayerNotExist() {
+        Player Vasya = new Player(1, "Вася", 100);
+        Game game = new Game();
+        game.registered(Vasya);
+        Assertions.assertThrows(NotRegisteredException.class,
+        () -> game.round("Жора","Вася")
+        );
+    }
+    @Test
+    public void testWhenSecondPlayerNotExist() {
+        Player Vasya = new Player(1, "Вася", 100);
+        Game game = new Game();
+        game.registered(Vasya);
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> game.round("Вася", "Жора")
+        );
+    }
+
+
 }
